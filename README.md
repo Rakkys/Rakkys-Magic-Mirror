aa9d36ad69b91d8f6ecbbcc6a1be7b847f2abc5f
Rakks <rakks.ejk@gmail.com>
2024-12-31 17:42:55 -0700

    added 'rakkys-' to the start of each gamerule to make it easier to find

--- README.md
+++ README.md
@@ -1,22 +1,23 @@
 # Rakkys' Magic Mirror
 A small, configurable, mod that adds the magic mirror from Terraria
+<br>[Modrinth](https://modrinth.com/mod/rakkys-magic-mirror)
 
 ## Features
 - Magic Mirror: Right clicking allows the user to teleport back to their spawn position
 - Ice Mirror: The same as the Magic Mirror but with a different recipe and texture
 ## Gamerules
-#### instantMagicMirror 
+#### rakkys-instantMagicMirror 
 - Determines if you have to charge the magic mirror a second before using it
 - Default: `false`
-#### magicMirrorCooldown 
+#### rakkys-magicMirrorCooldown 
 - Determines how long the cooldown between each mirror use is, in ticks
 - Default: `20`
 - Minimum: `20`
-#### mirrorExperienceLevelUsage
+#### rakkys-mirrorExperienceLevelUsage
 - Determines how many experience levels teleporting costs.
 - Default: `0`
 - Minimum: `0`
-#### mirrorHomeDimensionOnly
+#### rakkys-mirrorHomeDimensionOnly
 - Determines if you can only teleport if you're in your home dimension
 - Default: `false`
 ## Issues
--- src/main/java/net/rakkys/mirror/registries/GameRulesRegistry.java
+++ src/main/java/net/rakkys/mirror/registries/GameRulesRegistry.java
@@ -6,22 +6,22 @@ import net.minecraft.world.GameRules;
 
 public class GameRulesRegistry {
     public static final GameRules.Key<GameRules.BooleanRule> INSTANT_MAGIC_MIRROR =
-            GameRuleRegistry.register("instantMagicMirror",
+            GameRuleRegistry.register("rakkys-instantMagicMirror",
                     GameRules.Category.PLAYER,
                     GameRuleFactory.createBooleanRule(false));
 
     public static final GameRules.Key<GameRules.IntRule> MAGIC_MIRROR_COOLDOWN =
-            GameRuleRegistry.register("magicMirrorCooldown",
+            GameRuleRegistry.register("rakkys-magicMirrorCooldownTicks",
                     GameRules.Category.PLAYER,
                     GameRuleFactory.createIntRule(20, 20));
 
     public static final GameRules.Key<GameRules.IntRule> MIRROR_EXPERIENCE_LEVEL_USAGE =
-            GameRuleRegistry.register("mirrorExperienceLevelUsage",
+            GameRuleRegistry.register("rakkys-mirrorExperienceLevelUsage",
                     GameRules.Category.PLAYER,
                     GameRuleFactory.createIntRule(0, 0));
 
     public static final GameRules.Key<GameRules.BooleanRule> MIRROR_HOME_DIMENSION_ONLY =
-            GameRuleRegistry.register("mirrorHomeDimensionOnly",
+            GameRuleRegistry.register("rakkys-mirrorHomeDimensionOnly",
                     GameRules.Category.PLAYER,
                     GameRuleFactory.createBooleanRule(false));
 
