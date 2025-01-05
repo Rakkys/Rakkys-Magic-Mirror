7e464781ac9f2bcf409d93f57fd28491b4e7098c
Rakks <rakks.ejk@gmail.com>
2025-01-05 12:21:01 -0700

    extracted checks for use in other classes

--- src/main/java/net/rakkys/mirror/util/MirrorTeleportation.java
+++ src/main/java/net/rakkys/mirror/util/MirrorTeleportation.java
@@ -11,6 +11,7 @@ import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.rakkys.mirror.registries.GameRulesRegistry;
 
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 
@@ -19,43 +20,68 @@ public class MirrorTeleportation {
         teleportPlayerToSpawn(player, false);
     }
 
-    public static void teleportPlayerToSpawn(ServerPlayerEntity player, boolean decreaseExperience) {
+    public static boolean teleportPlayerToSpawn(ServerPlayerEntity player, boolean decreaseExperience) {
         BlockPos spawnPos = player.getSpawnPointPosition();
         RegistryKey<World> spawnWorldKey = player.getSpawnPointDimension();
         ServerWorld spawnWorld = player.getServer().getWorld(spawnWorldKey);
 
+        Objects.requireNonNull(spawnWorld);
 
-        if (spawnPos == null || spawnWorld == null) {
-            player.sendMessage(Text.translatable("text.rakkys-mirror.error.null_spawn_args"), true);
-            return;
-        }
-
-        Optional<Vec3d> userSpawn = PlayerEntity.findRespawnPosition(spawnWorld, spawnPos, 0f, false, player.isAlive());
-        if (userSpawn.isEmpty()) {
-            player.sendMessage(Text.translatable("text.rakkys-mirror.error.null_spawn_args"), true);
-            return;
-        }
-
-        if (player.getWorld().getGameRules().getBoolean(GameRulesRegistry.MIRROR_HOME_DIMENSION_ONLY)) {
-            if (player.getWorld() != spawnWorld && !player.isCreative()) {
-                player.sendMessage(Text.translatable("text.rakkys-mirror.error.not_home_dimension"), true);
-                return;
-            }
-        }
-
-        if (decreaseExperience && !player.isCreative()) {
-            int experienceUsage = player.getWorld().getGameRules().getInt(GameRulesRegistry.MIRROR_EXPERIENCE_LEVEL_USAGE);
-            if (player.experienceLevel >= experienceUsage) {
-                player.setExperienceLevel(player.experienceLevel - experienceUsage);
-            } else {
-                player.sendMessage(Text.translatable("text.rakkys-mirror.error.not_enough_experience"), true);
-                return;
-            }
+        if (!canTeleport(spawnPos, spawnWorld, player, decreaseExperience)) {
+            return false;
         }
 
         Set<PositionFlag> flags = Set.of(PositionFlag.X, PositionFlag.Y, PositionFlag.Z);
         player.teleport(spawnWorld,
                 spawnPos.getX(), spawnPos.getY(), spawnPos.getZ(),
                 flags, player.getYaw(), player.getPitch());
+
+        return true;
+    }
+
+    public static boolean canTeleport(BlockPos spawnPos, ServerWorld spawnWorld, ServerPlayerEntity player, boolean decreaseExperience) {
+        return checkValidRespawnPos(spawnPos, spawnWorld, player) &&
+                canTeleportOtherDimension(spawnWorld, player) &&
+                runExperienceCheck(decreaseExperience, player);
+    }
+
+    public static boolean checkValidRespawnPos(BlockPos spawnPos, ServerWorld spawnWorld, ServerPlayerEntity player) {
+        if (spawnPos == null || spawnWorld == null) {
+            player.sendMessage(Text.translatable("text.rakkys-mirror.error.null_spawn_args"), true);
+            return false;
+        }
+
+        Optional<Vec3d> userSpawn = PlayerEntity.findRespawnPosition(spawnWorld, spawnPos, 0f, false, player.isAlive());
+        if (userSpawn.isEmpty()) {
+            player.sendMessage(Text.translatable("text.rakkys-mirror.error.null_spawn_args"), true);
+            return false;
+        }
+
+        return true;
+    }
+
+    public static boolean canTeleportOtherDimension(ServerWorld spawnWorld, ServerPlayerEntity player) {
+        if (player.getWorld().getGameRules().getBoolean(GameRulesRegistry.MIRROR_HOME_DIMENSION_ONLY)) {
+            if (player.getWorld() != spawnWorld && !player.isCreative()) {
+                player.sendMessage(Text.translatable("text.rakkys-mirror.error.not_home_dimension"), true);
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean runExperienceCheck(boolean decreaseExperience, ServerPlayerEntity player) {
+        if (decreaseExperience && !player.isCreative()) {
+            int experienceUsage = player.getWorld().getGameRules().getInt(GameRulesRegistry.MIRROR_EXPERIENCE_LEVEL_USAGE);
+            if (player.experienceLevel >= experienceUsage) {
+                player.setExperienceLevel(player.experienceLevel - experienceUsage);
+                return true;
+            } else {
+                player.sendMessage(Text.translatable("text.rakkys-mirror.error.not_enough_experience"), true);
+                return false;
+            }
+        }
+        return true;
     }
 }
